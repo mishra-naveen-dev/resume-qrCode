f8bd7b8dbf116147ac1420fdb04dc444b6eebef9
nvineng <mishra.naveen.dev@gmail.com>
2025-04-13 23:30:57 +0530

    update the link again

--- qr-scanner.py
+++ qr-scanner.py
@@ -4,9 +4,11 @@ import qrcode
 
 
 # Fixed redirection URL or backend API endpoint
-redirect_url = "https://drive.google.com/file/d/1vbfaoglO4cvmt1Zn00vJa2Fpl7jjzYK8/view?usp=drive_link"
+redirect_url = "https://drive.google.com/file/d/11k3ofuVJZgGjjEv2jFqkM4Wci8NoC4y_/view?usp=drive_link"
 
 # Generate the QR code
 myQR = qrcode.make(redirect_url)
-myQR.save("qr-code.png")  # Save the QR code
+myQR.save("qr-code.webp")  # Save the QR code
 
+# run command
+# python qr-scanner.py
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,2 @@
-myQR=qrcode.make("https://drive.google.com/file/d/1vbfaoglO4cvmt1Zn00vJa2Fpl7jjzYK8/view?usp=drive_link")
+myQR=qrcode.make("https://drive.google.com/file/d/11k3ofuVJZgGjjEv2jFqkM4Wci8NoC4y_/view?usp=drive_link")
 # myQR.save("qr-code.png")  # Change file format to PNG
